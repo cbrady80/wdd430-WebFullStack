34e10db43dd5117ea7f1e2f507da666fce4f4683
Cheryl Brady <cbrady80@byui.edu>
2024-01-20 20:46:31 -0700

    Chapter 3 completed

---
+++ tutorial-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Testing testing', 
+    'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+    new Recipe('Test Recipe', 'Testing testing', 
+    'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg')
+  ];
+}
