3119c04ab8dc18fd2895d861ec798854b992bd64
Cheryl Brady <cbrady80@byui.edu>
2024-01-26 16:53:05 -0700

    Finished chapter 6 tutorial week 3

--- tutorial-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ tutorial-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'Testing testing', 
     'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
-    new Recipe('Test Recipe', 'Testing testing', 
+    new Recipe('Another Test Recipe', 'something else', 
     'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg')
   ];
+
+  constructor() { }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
